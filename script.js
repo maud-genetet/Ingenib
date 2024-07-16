a6322410d2546fac65f15cf055101fcac0fb497b
maud-genetet <maud.genetet@gmail.com>
2024-07-16 23:05:54 +0200

    bug

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const filieres = ["E", "M", "T", "I", "RSI", "SEE", "IHS", "ER", "F"];
 const couleursFilieres = ["#337ab7", "#ff2929", "#e8c243", "#006600", "#ff66cc", "#9933ff", "#88ff75", "#eda13e", "#200766"];
 const iconFilieres = ["fa-microchip", "fa-area-chart", "fa-wifi", "fa-laptop", "fa-sitemap", "fa-code-fork", "fa-universal-access", "fa-eye", "fa-users"];
 const buttonFilieres = document.querySelectorAll('.btn');
+const btnVoirPlus = document.querySelectorAll('.btn-voir-plus');
 
 function getFiliere(filière) {
     return `<span class="fa-stack" data-toggle="tooltip" data-placement="bottom">
@@ -20,15 +21,24 @@ function getDivWithInfo(colonne, texte) {
     return "";
 }
 
-if (buttonFilieres) {
-    buttonFilieres.forEach(b => {
-        b.addEventListener('click', function () {
-            document.querySelector('.stage').innerHTML = "";
-            chargeElement(b.id);
-        });
+buttonFilieres.forEach(b => {
+    b.addEventListener('click', function () {
+        document.querySelector('.stage').innerHTML = "";
+        chargeElement(b.id);
     });
-}
+});
 
+btnVoirPlus.forEach(b => {
+    b.addEventListener('click', function () {
+        const contenu = document.getElementById("contenu_" + b.id.split("_")[1]);
+        if (contenu.style.display == "none") {
+            contenu.style.display = "flex";
+            b.textContent = "Voir moins";
+        } else {
+            contenu.style.display = "none";
+            b.textContent = "Voir plus";
+        }
+    });
 
 
 function chargeElement(type_filiere) {
@@ -61,16 +71,8 @@ function chargeElement(type_filiere) {
                             h3.textContent = row['Entreprises'];
                             h3.style = "margin: 5px;";
 
-                            if (row['Site'] != "") {
-                                const lienSite = document.createElement('a');
-                                lienSite.href = row['Site'];
-                                lienSite.textContent = "Site Web";
-                                lienSite.target = "_blank";
-                                lienSite.style = "margin: 10px; color: white; background-color: rgb(139 198 62); padding: 5px; border-radius: 5px;";
-                            }
-
                             const contenu = document.createElement('div');
-                            contenu.style = "display: flex; flex-direction: column; width: 100%;";
+                            contenu.style = "display: none; flex-direction: column; width: 100%;";
                             contenu.id = "contenu_" + row['Entreprises'];
                             
                             contenu.innerHTML += getDivWithInfo("Secteurs", row["Secteurs"]);
@@ -81,17 +83,10 @@ function chargeElement(type_filiere) {
                             contenu.innerHTML += getDivWithInfo("Evolutions", row["Evolutions"]);
 
                             const buttonVoirPlus = document.createElement('button');
-                            buttonVoirPlus.className = "btn btn-primary";
+                            buttonVoirPlus.className = 'btn-voir-plus';
                             buttonVoirPlus.textContent = "Voir plus";
                             buttonVoirPlus.style = "margin: 10px;";
-                            buttonVoirPlus.addEventListener('click', function () {
-                                const contenu = document.getElementById("contenu_" + row['Entreprises']);
-                                if (contenu.style.display == "none") {
-                                    contenu.style.display = "flex";
-                                } else {
-                                    contenu.style.display = "none";
-                                }
-                            });
+                            buttonVoirPlus.id = "btn_" + row['Entreprises'];
                             
                             const filiere = document.createElement('div');
                             filiere.style = "margin: 5px;";
@@ -105,6 +100,11 @@ function chargeElement(type_filiere) {
                             captionDiv.appendChild(h3);
                             captionDiv.appendChild(filiere);
                             if (row['Site'] != ""){
+                                const lienSite = document.createElement('a');
+                                lienSite.href = row['Site'];
+                                lienSite.textContent = "Site Web";
+                                lienSite.target = "_blank";
+                                lienSite.style = "margin: 10px; color: white; background-color: rgb(139 198 62); padding: 5px; border-radius: 5px;";
                                 captionDiv.appendChild(lienSite);
                             }
                             captionDiv.appendChild(buttonVoirPlus);
